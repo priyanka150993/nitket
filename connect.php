2fe4936912b06a3f2b0da290ff7bad356141c1d0
amit kumar soni <amitsoniforbhu@gmail.com>
2017-03-21 19:37:50 +0530

    Connection File With Server
    Connection File With Server

--- connect.php
+++ connect.php
@@ -1,1 +1,5 @@
-<?php $conn = new mysqli('localhost','root','amit','nit-ket'); ?>
+
+
+<?php 
+//This File is Having Connection Variable and Setup Done By Amit //
+$conn = new mysqli('localhost','root','amit','nit-ket'); ?>
